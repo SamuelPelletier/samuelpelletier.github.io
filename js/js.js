c3446f4bdf64bf605fcd4d5996ba328db6ee44b8
PELLETIER Samuel <pellesam.info@gmail.com>
2019-09-04 11:39:31 +0200

    Add "savoir être" and contact

--- js/js.js
+++ js/js.js
@@ -194,7 +194,7 @@ var chart = AmCharts.makeChart("chartdiv2", {
     "valueAxes": [{
         "axisAlpha": 0,
         "position": "left",
-        "title": "Langages de programations",
+        "title": "Langages de programmations",
         "maximum": 10,
         "minimum": 0
     }],
@@ -257,5 +257,47 @@ $.fn.commentCards = function () {
 $('.cards').commentCards();
 
 
+$(document).ready(function () {
 
+    var
+        words = $('#tagi text'),
+        l = words.length,
+        current = null,
+        delay = 2000;
 
+    function clearBlink(o) {
+        var
+            ca = o.getAttribute('class').split(' '),
+            i = ca.indexOf('blink');
+
+        if (i !== -1) {
+            ca.splice(i, 1);
+            o.setAttribute('class', ca.join(' '));
+        }
+    }
+
+    function addBlink(o) {
+        var
+            ca = o.getAttribute('class').split(' ');
+        ca.push('blink');
+        o.setAttribute('class', ca.join(' '));
+    }
+
+    function wordblink() {
+
+        var e;
+
+        if (current !== null) {
+            clearBlink(words.eq(current)[0])
+        }
+
+        current = Math.floor(Math.random() * l);
+        e = words.eq(current);
+        addBlink(e[0]);
+
+        setTimeout(wordblink, delay);
+    }
+
+    wordblink();
+
+});
